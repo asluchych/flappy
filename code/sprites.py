6dbdb5058a999f8246e3353b2fa55e619cc7e6eb
asluchych <anatolblr@gmail.com>
2024-02-27 17:55:36 +0100

    Add scale factor

--- code/sprites.py
+++ code/sprites.py
@@ -3,7 +3,12 @@ from pygame.sprite import Group
 from settings import *
 
 class BG(pygame.sprite.Sprite):
-    def __init__(self, groups):
+    def __init__(self, groups, scale_factor):
         super().__init__(groups)
-        self.image = pygame.image.load('../graphics/environment/background.png').convert()
+        bg_image = pygame.image.load('./graphics/environment/background.png').convert()
+
+        full_height = bg_image.get_height() * scale_factor
+        full_width = bg_image.get_width() * scale_factor
+
+        self.image = pygame.transform.scale(bg_image, (full_width, full_height))
         self.rect = self.image.get_rect(topleft = (0,0))
